f632490d7e16911eee85a401225d5b7f920a6e8c
Hector Vargas <vargas.rodriguez.vicente1b@gmail.com>
2025-04-07 22:27:52 -0600

    Primer commit

---
+++ SolucionGuarderia/Models/GuarderiaContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SolucionGuarderia.Models;
+
+public partial class GuarderiaContext : DbContext
+{
+    public GuarderiaContext()
+    {
+    }
+
+    public GuarderiaContext(DbContextOptions<GuarderiaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        //=> optionsBuilder.UseSqlServer("server=localhost; database=Guarderia; Trusted_Connection=SSPI; Encrypt=false; TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Usuarios__3214EC075E01F635");
+
+            entity.HasIndex(e => e.CorreoElectronico, "UQ__Usuarios__531402F31734FF6E").IsUnique();
+
+            entity.Property(e => e.Activo).HasDefaultValue(true);
+            entity.Property(e => e.Apellido).HasMaxLength(100);
+            entity.Property(e => e.Contrasena).HasMaxLength(255);
+            entity.Property(e => e.CorreoElectronico).HasMaxLength(150);
+            entity.Property(e => e.FechaRegistro)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.NivelUsuario)
+                .HasMaxLength(100)
+                .HasColumnName("Nivel_Usuario");
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.NombreUsuario)
+                .HasMaxLength(100)
+                .HasColumnName("Nombre_Usuario");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ SolucionGuarderia/Models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolucionGuarderia.Models;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string NombreUsuario { get; set; } = null!;
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellido { get; set; } = null!;
+
+    public string CorreoElectronico { get; set; } = null!;
+
+    public string Contrasena { get; set; } = null!;
+
+    public string NivelUsuario { get; set; } = null!;
+
+    public DateTime? FechaRegistro { get; set; }
+
+    public bool? Activo { get; set; }
+}
